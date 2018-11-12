991b383a2b600254fc87beac8ad86963f2e890f4
icw2 <icw2@FSTU603003.pti.edu>
2018-11-12 15:05:15 -0500

    Here's a Thing.

---
+++ src/ucdmidtermtest/UCDMidtermTest.java
@@ -0,0 +1,14 @@
+/*
+ * Ian Walker
+11/12/18
+ */
+package ucdmidtermtest;
+
+public class UCDMidtermTest {
+
+    public static void main(String args[]) 
+    { 
+        for (int x = 1; x <= 100; x++) 
+            System.out.println(x + " is a Cool Number."); 
+    } 
+}
